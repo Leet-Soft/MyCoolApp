4dca07179cdb99aa725a5db2246dcc033e010999
Leet-Soft <jtodorov@leet-soft.com>
2021-01-30 15:18:34 +0200

    Remote access test

--- app/src/main/java/uni/fmi/masters/mycoolapp/LoginActivity.java
+++ app/src/main/java/uni/fmi/masters/mycoolapp/LoginActivity.java
@@ -2,14 +2,29 @@ package uni.fmi.masters.mycoolapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -39,15 +54,7 @@ public class LoginActivity extends AppCompatActivity {
 
                 String usernameInput = usernameET.getText().toString();
                 String passwordInput = passwordET.getText().toString();
-
-                if(true){
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    intent.putExtra("user", usernameInput);
-
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(), "HACKER GO AWAY!!!", Toast.LENGTH_SHORT).show();
-                }
+                new LoginAsynTask(usernameInput, passwordInput).execute();
 
             }else{
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
@@ -55,4 +62,81 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
     };
+
+    private class LoginAsynTask extends AsyncTask<Void, Void, Void> {
+
+        String username;
+        String password;
+        int loggedUserID = -1;
+
+        ProgressDialog dialog;
+
+        LoginAsynTask(String username, String password){
+            this.username = username;
+            this.password = password;
+            dialog = new ProgressDialog(LoginActivity.this);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            dialog.setTitle("Login in...");
+            dialog.show();
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            String urlString = String.format("http://78.130.252.70:8989/Login?username=%s&password=%s",
+                    username, password);
+
+            HttpURLConnection urlConnection = null;
+            try {
+                URL url = new URL(urlString);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream stream = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line = reader.readLine();
+
+                if(line != null && !line.equals("")){
+                    JSONObject object = new JSONObject(line);
+                    loggedUserID = object.getInt("ID");
+                }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+                e.printStackTrace();
+        } catch (JSONException e) {
+                e.printStackTrace();
+        } finally {
+            if(urlConnection != null)
+                urlConnection.disconnect();
+        }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            dialog.hide();
+
+            if(loggedUserID != -1){
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+                intent.putExtra("userID", loggedUserID);
+                startActivity(intent);
+            }else{
+                Toast.makeText(LoginActivity.this, "Wrong credentials!", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
 }
--- app/src/main/java/uni/fmi/masters/mycoolapp/RegisterActivity.java
+++ app/src/main/java/uni/fmi/masters/mycoolapp/RegisterActivity.java
@@ -3,18 +3,30 @@ package uni.fmi.masters.mycoolapp;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -83,10 +95,6 @@ public class RegisterActivity extends AppCompatActivity {
                         && passwordET.getText().length() > 0
                         && passwordET.getText().toString().equals(repeatPasswordET.getText().toString())){
 
-                    //editor.putString(SHARED_PREF_USERNAME, usernameET.getText().toString());
-                    // editor.putString(SHARED_PREF_PASSWORD, passwordET.getText().toString());
-                    //editor.commit();
-
                     String username = usernameET.getText().toString();
                     String password = passwordET.getText().toString();
                     String firstName = firstNameET.getText().toString();
@@ -103,10 +111,7 @@ public class RegisterActivity extends AppCompatActivity {
                     user.setLastName(lastName);
                     user.setGender(gender);
 
-                    if(true){
-                        Toast.makeText(RegisterActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
+                    new RegisterAsyncTask(user).execute();
 
                 }else{
                     Toast.makeText(RegisterActivity.this,
@@ -142,4 +147,74 @@ public class RegisterActivity extends AppCompatActivity {
             avatarIV.setImageBitmap(avatarImage);
         }
     }
+
+    private class RegisterAsyncTask extends AsyncTask<Void, Void, Void>{
+
+        User user;
+        boolean isSuccess = false;
+
+        ProgressDialog dialog;
+
+        RegisterAsyncTask(User user){
+            this.user = user;
+            dialog = new ProgressDialog(RegisterActivity.this);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog.setTitle("Registration in progress...");
+            dialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            String urlString = String.format("http://78.130.252.70:8989/RegisterUser?" +
+                    "username=%s&password=%s&gender=%s&fname=%s&lname=%s", user.getUsername(),
+                    user.getPassword(), user.getGender(), user.getFirstName(), user.getLastName());
+
+            HttpURLConnection urlConnection = null;
+            try{
+                URL url = new URL(urlString);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream stream = new BufferedInputStream(urlConnection.getInputStream());
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                String result = reader.readLine();
+
+                if(result != null){
+                    if(result.contains("true")){
+                        isSuccess = true;
+                    }
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            dialog.hide();
+
+            if(isSuccess){
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }else{
+                Toast.makeText(RegisterActivity.this, "Something went wrong!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
